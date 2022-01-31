0cec0003b0f460f8ed6d74e7506231bc763f4037
ziga <ziga.zajc007@gmail.com>
2022-01-31 03:26:56 +0100

    Added latency tracker

--- js/index.js
+++ js/index.js
@@ -37,7 +37,7 @@ const servers = {
 
 let srvHtml = "";
 for(let i = 0; i < Object.keys(servers).length; i++){
-    srvHtml += "<tr><td class='px-6 py-4 whitespace-nowrap'><div class='flex items-center'><div class='flex-shrink-0 h-10 w-10'><img class='h-10 w-10 rounded-full' src='images/flags/" + Object.values(servers)[i]["location"] + ".png' alt='" + Object.values(servers)[i]["location"] + " flag'></div><div class='ml-4'><div class='text-sm font-medium text-gray-300'>" + Object.keys(servers)[i] + "</div><div class='text-sm text-gray-400'>" + Object.values(servers)[i]["domain"] + "</div></div></div></td><td id='srv-accounts-" + i + "' class='px-6 py-4 whitespace-nowrap text-sm text-gray-400'>0/0</td><td id='srv-version-" + i + "' class='px-6 py-4 whitespace-nowrap text-sm text-gray-400'>/</td><td class='px-6 py-4 whitespace-nowrap'><span id='srv-status-" + i + "' class='px-2 inline-flex text-xs leading-5 font-semibold rounded-md bg-red-800 text-red-300'>Offline</span></td></tr>";
+    srvHtml += "<tr><td class='px-6 py-4 whitespace-nowrap'><div class='flex items-center'><div class='flex-shrink-0 h-10 w-10'><img class='h-10 w-10 rounded-full' src='images/flags/" + Object.values(servers)[i]["location"] + ".png' alt='" + Object.values(servers)[i]["location"] + " flag'></div><div class='ml-4'><div class='text-sm font-medium text-gray-300'>" + Object.keys(servers)[i] + "</div><div class='text-sm text-gray-400'>" + Object.values(servers)[i]["domain"] + "</div></div></div></td><td id='srv-accounts-" + i + "' class='px-6 py-4 whitespace-nowrap text-sm text-gray-400'>0/0</td><td id='srv-version-" + i + "' class='px-6 py-4 whitespace-nowrap text-sm text-gray-400'>/</td><td class='px-6 py-4 whitespace-nowrap'><span id='srv-latency-" + i + "' class='px-2 inline-flex text-xs leading-5 font-semibold rounded-md bg-red-800 text-red-300'>0 ms</span></td><td class='px-6 py-4 whitespace-nowrap'><span id='srv-status-" + i + "' class='px-2 inline-flex text-xs leading-5 font-semibold rounded-md bg-red-800 text-red-300'>Offline</span></td></tr>";
 }
 
 document.getElementById("public-servers-table").innerHTML = srvHtml;
@@ -109,6 +109,7 @@ document.getElementById("zec-qr").addEventListener("click", () => {
 //Public Server Info
 for(let i = 0; i < Object.values(servers).length; i++){
     let jsonData = localStorage.getItem("data-" + i);
+    let latency = localStorage.getItem("latency-" + i);
     let jsonTime = localStorage.getItem("time-" + i);
     
     if(typeof(jsonData) == 'undefined' || jsonData == null){
@@ -125,6 +126,7 @@ for(let i = 0; i < Object.values(servers).length; i++){
         JSON.parse(jsonData);
     }catch (e){
         localStorage.setItem("data-" + i, "{}");
+        localStorage.setItem("latency-" + i, 0);
         localStorage.setItem("time-" + i, 0);
         fetchServerInfo(i);
         continue;
@@ -135,24 +137,52 @@ for(let i = 0; i < Object.values(servers).length; i++){
         continue;
     }
 
+    if(Number(latency) <= 0){
+        fetchServerInfo(i);
+        continue;
+    }
+
     let json = JSON.parse(jsonData);
     document.getElementById("srv-accounts-" + i).innerText = json.users + "/" + json.maxUsers;
     document.getElementById("srv-version-" + i).innerText = json.version;
+    document.getElementById("srv-latency-" + i).innerText = Number(latency) + " ms";
+    if(Number(latency) > 200){
+        document.getElementById("srv-latency-" + i).className = "px-2 inline-flex text-xs leading-5 font-semibold rounded-md bg-red-800 text-red-300";
+    }else if(Number(latency) > 100){
+        document.getElementById("srv-latency-" + i).className = "px-2 inline-flex text-xs leading-5 font-semibold rounded-md bg-yellow-800 text-red-200";
+    }else{
+        document.getElementById("srv-latency-" + i).className = "px-2 inline-flex text-xs leading-5 font-semibold rounded-md bg-green-800 text-green-500";
+    }
     document.getElementById("srv-status-" + i).innerText = "Online";
     document.getElementById("srv-status-" + i).className = "px-2 inline-flex text-xs leading-5 font-semibold rounded-md bg-green-800 text-green-500";
 }
 
 function fetchServerInfo(i){
+    const latencyStart = new Date();
+    let latency = 0;
     fetch(Object.values(servers)[i].domain + "?action=getInfo")
     .then(response => {
-        if (response.ok) return response.json();
+        if (response.ok){
+            latency = new Date() - latencyStart;
+            return response.json();
+        }
         localStorage.setItem("data-" + i, "{}");
+        localStorage.setItem("latency-" + i, 0);
         localStorage.setItem("time-" + i, 0);
     }).then(json => {
         localStorage.setItem("data-" + i, JSON.stringify(json));
+        localStorage.setItem("latency-" + i, latency);
         localStorage.setItem("time-" + i, Date.now());
         document.getElementById("srv-accounts-" + i).innerText = json.users + "/" + json.maxUsers;
         document.getElementById("srv-version-" + i).innerText = json.version;
+        document.getElementById("srv-latency-" + i).innerText = latency + " ms";
+        if(Number(latency) > 200){
+            document.getElementById("srv-latency-" + i).className = "px-2 inline-flex text-xs leading-5 font-semibold rounded-md bg-red-800 text-red-300";
+        }else if(Number(latency) > 100){
+            document.getElementById("srv-latency-" + i).className = "px-2 inline-flex text-xs leading-5 font-semibold rounded-md bg-yellow-800 text-red-200";
+        }else{
+            document.getElementById("srv-latency-" + i).className = "px-2 inline-flex text-xs leading-5 font-semibold rounded-md bg-green-800 text-green-500";
+        }
         document.getElementById("srv-status-" + i).innerText = "Online";
         document.getElementById("srv-status-" + i).className = "px-2 inline-flex text-xs leading-5 font-semibold rounded-md bg-green-800 text-green-500";
     }).catch();
