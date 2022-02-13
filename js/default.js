62116d7a9b4d4b1827b536e864d9f1dd95f32c25
ziga <ziga.zajc007@gmail.com>
2022-02-13 20:55:45 +0100

    Speed up website loading time

--- js/default.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("main-menu-btn").addEventListener("click", () => {
-  if(document.getElementById("mobile-menu").className == "sm:hidden hidden"){
-      document.getElementById("mobile-menu").className = "sm:hidden";
-      document.getElementById("main-menu-icon").innerHTML = "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M6 18L18 6M6 6l12 12' />";
-      return;
-  }
-  document.getElementById("mobile-menu").className = "sm:hidden hidden";
-  document.getElementById("main-menu-icon").innerHTML = "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M4 6h16M4 12h16M4 18h16' />";
-});
