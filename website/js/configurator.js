999c67c75758851178802b1c2a999cae69b69e22
Ziga Zajc <ziga.zajc007@gmail.com>
2024-08-07 23:39:30 +0200

    Added Download Configuration button

--- website/js/configurator.js
+++ website/js/configurator.js
@@ -44,3 +44,81 @@ document.getElementById("database-mysql").addEventListener("change", () => {
 		mysqlSettings.classList.remove("hidden");
 	}
 });
+
+function getValue(id) {
+	return document.getElementById(id).value;
+}
+
+function getStatus(id) {
+	return document.getElementById(id).checked;
+}
+
+function generateEnv() {
+	let databaseEngine = getStatus("database-mysql") ? "mysql" : "sqlite";
+
+	return `#
+#	Passky Server Configuration
+# https://passky.org/configurator
+#	${new Date().toISOString()}
+#
+
+SERVER_CORES=${getValue("server-cores")}
+SERVER_LOCATION=${getValue("server-country")}
+ADMIN_USERNAME="${getValue("admin-username")}"
+ADMIN_PASSWORD="${getValue("admin-password")}"
+CF_TURNSTILE_SITE_KEY=${getValue("admin-captcha-sitekey")}
+CF_TURNSTILE_SECRET_KEY=${getValue("admin-captcha-secretkey")}
+YUBI_CLOUD=${getValue("yubi-cloud")}
+YUBI_ID=${getValue("yubi-id")}
+ACCOUNT_MAX=${getValue("account-max")}
+ACCOUNT_MAX_PASSWORDS=${getValue("account-password-limit")}
+ACCOUNT_PREMIUM=${getValue("account-premium")}
+DATABASE_ENGINE=${databaseEngine}
+DATABASE_FILE="${getValue("sqlite-name")}"
+MYSQL_CACHE_MODE=${getValue("database-cache-type")}
+MYSQL_HOST="${getValue("mysql-host")}"
+MYSQL_PORT=${getValue("mysql-port")}
+MYSQL_DATABASE="${getValue("mysql-database")}"
+MYSQL_USER="${getValue("mysql-user")}"
+MYSQL_PASSWORD="${getValue("mysql-password")}"
+MYSQL_SSL=${getStatus("mysql-ssl")}
+MYSQL_SSL_CA="${getValue("mysql-ca-certificate")}"
+REDIS_HOST="${getValue("redis-internal-host")}"
+REDIS_PORT=${getValue("redis-internal-port")}
+REDIS_PASSWORD="${getValue("redis-internal-password")}"
+REDIS_LOCAL_HOST="${getValue("redis-external-host")}"
+REDIS_LOCAL_PORT=${getValue("redis-external-port")}
+REDIS_LOCAL_PASSWORD="${getValue("redis-external-password")}"
+MAIL_ENABLED=${getStatus("email-smtp")}
+MAIL_HOST="${getValue("smtp-host")}"
+MAIL_PORT=${getValue("smtp-port")}
+MAIL_USERNAME="${getValue("smtp-username")}"
+MAIL_PASSWORD="${getValue("smtp-password")}"
+MAIL_USE_TLS=${getStatus("smtp-tls")}
+LIMITER_ENABLED=${getStatus("limiter-enabled")}
+LIMITER_GET_INFO=${getValue("limiter-getInfo")}
+LIMITER_GET_STATS=${getValue("limiter-getStats")}
+LIMITER_GET_REPORT=${getValue("limiter-getReport")}
+LIMITER_GET_TOKEN=${getValue("limiter-getToken")}
+LIMITER_GET_PASSWORDS=${getValue("limiter-getPasswords")}
+LIMITER_SAVE_PASSWORD=${getValue("limiter-savePassword")}
+LIMITER_EDIT_PASSWORD=${getValue("limiter-editPassword")}
+LIMITER_DELETE_PASSWORD=${getValue("limiter-deletePassword")}
+LIMITER_DELETE_PASSWORDS=${getValue("limiter-deletePasswords")}
+LIMITER_CREATE_ACCOUNT=${getValue("limiter-createAccount")}
+LIMITER_DELETE_ACCOUNT=${getValue("limiter-deleteAccount")}
+LIMITER_IMPORT_PASSWORDS=${getValue("limiter-importPasswords")}
+LIMITER_ENABLE_2FA=${getValue("limiter-enable2fa")}
+LIMITER_DISABLE_2FA=${getValue("limiter-disable2fa")}
+LIMITER_ADD_YUBIKEY=${getValue("limiter-addYubiKey")}
+LIMITER_REMOVE_YUBIKEY=${getValue("limiter-removeYubiKey")}
+LIMITER_UPGRADE_ACCOUNT=${getValue("limiter-upgradeAccount")}
+LIMITER_FORGOT_USERNAME=${getValue("limiter-forgotUsername")}
+`;
+}
+
+document.getElementById("download-configuration-btn").addEventListener("click", () => {
+	const name = `${Date.now()}.env`;
+	downloadEnv(generateEnv(), name);
+	alert(`The configuration file ${name} has been downloaded. Please rename it to .env before uploading it to the server.`);
+});
--- website/js/default.js
+++ website/js/default.js
@@ -1,9 +1,19 @@
 document.getElementById("main-menu-btn").addEventListener("click", () => {
-  if(document.getElementById("mobile-menu").className == "sm:hidden hidden"){
-      document.getElementById("mobile-menu").className = "sm:hidden";
-      document.getElementById("main-menu-icon").innerHTML = "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M6 18L18 6M6 6l12 12' />";
-      return;
-  }
-  document.getElementById("mobile-menu").className = "sm:hidden hidden";
-  document.getElementById("main-menu-icon").innerHTML = "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M4 6h16M4 12h16M4 18h16' />";
+	if (document.getElementById("mobile-menu").className == "sm:hidden hidden") {
+		document.getElementById("mobile-menu").className = "sm:hidden";
+		document.getElementById("main-menu-icon").innerHTML = "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M6 18L18 6M6 6l12 12' />";
+		return;
+	}
+	document.getElementById("mobile-menu").className = "sm:hidden hidden";
+	document.getElementById("main-menu-icon").innerHTML = "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M4 6h16M4 12h16M4 18h16' />";
 });
+
+function downloadEnv(exportTxt, exportName) {
+	let dataStr = "data:application/x-empty;charset=utf-8," + encodeURIComponent(exportTxt);
+	let downloadAnchorNode = document.createElement("a");
+	downloadAnchorNode.setAttribute("href", dataStr);
+	downloadAnchorNode.setAttribute("download", exportName);
+	document.body.appendChild(downloadAnchorNode); // required for firefox
+	downloadAnchorNode.click();
+	downloadAnchorNode.remove();
+}
